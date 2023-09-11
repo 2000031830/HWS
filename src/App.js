02e6ec7349df784ea390a377316a4ae272677f1b
2000031830 <2000031830@kluniversity.in>
2023-09-11 11:55:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
+import React from 'react';
+import Navbar from './components/Navbar';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Signup from "./components/pages/SignUp";
+import Login from './components/pages/Login';
+import Home from './components/pages/Home';
+import Records from './components/pages/Records';
+import Recommendtions from './components/pages/Recommendtions';
+import hospitalinfo from './components/pages/Hospital info';
+import RecordDetails from './components/pages/RecordDetails';
+import Feedback from './components/feedback'
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/records' component={Records} />
+          <Route path='/recorddetails' component={RecordDetails}/>
+          <Route path='/recommendations' component={Recommendtions} />
+          <Route path='/hospitalinfo' component={hospitalinfo} />
+          <Route path='/sign-up' component={Signup} />
+          <Route path='/login'  component={Login} />
+          <Route path='/feedback'  component={Feedback} />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
